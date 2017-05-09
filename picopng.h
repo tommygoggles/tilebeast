c58d9fbafd1052c0d767d789399282dd8ea00338
tommygoggles <tommygoggles@gmail.com>
2017-05-09 10:19:31 +0100

    Update picopng.h

--- picopng.h
+++ picopng.h
@@ -4,7 +4,7 @@
 #include <vector>
 
 #ifndef size_t
-#define size_t unsigned long
+#define size_t std::size_t
 #endif
 
 int decodePNG(std::vector<unsigned char>& out_image, unsigned long& image_width, unsigned long& image_height, const unsigned char* in_png, size_t in_size, bool convert_to_rgba32);
